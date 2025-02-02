700a50e29cfc4fb432a10ee96ecca8adf528ef44
Leonardo Bombardelli <leonardo.b.bombardelli@gmail.com>
2025-02-01 21:14:43 -0300

    First commit c:

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Current NixOS Setup
+
+This is a work in progress. I'm still learning how to use NixOS. Please please PLEASE don't judge me on how ugly, disorganized, and wrong all these files are. I'm dumb as a rock and I really know it's all kinda wrong and misconfigured. Any suggestions and corrections to my setup are welcome btw c:
---
+++ home.nix
@@ -0,0 +1,124 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "lbbombardelli";
+  home.homeDirectory = "/home/lbbombardelli";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+
+    spaceship-prompt
+  ];
+
+  # Setting up Git - https://nixos.wiki/wiki/Git
+  programs.git = {
+    enable = true;
+    userName  = "Leonardo Bombardelli";
+    userEmail = "leonardo.b.bombardelli@gmail.com";
+  };
+
+  # Setting up zshell
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "history"
+        "rust"
+        "pip"
+        "docker"
+        "docker-compose"
+        "golang"
+        "ssh"
+      ];
+    };
+   };
+
+
+}
